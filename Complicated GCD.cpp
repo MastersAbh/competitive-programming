fbe42945ab880d625505eda6f8a6159889dbf5b5
abhinav <mastersabhinav@gmail.com>
2018-09-02 15:24:27 +0530

    Codeforces -- Complicated GCD

---
+++ Complicated GCD.cpp
@@ -0,0 +1,40 @@
+/*
+        " What doesn't change is your code! "
+        Problem Statement : http://codeforces.com/contest/664/problem/A
+        
+        
+        Code By:
+            Abhinav
+            CSE, NIT Patna
+            2k15
+        Masters_Abh on Hackerrank,Codeforces & SPOJ
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+#define ulint unsigned long long int
+#define lint long long int
+#define mp make_pair
+#define pb push_back
+#define f(x) for(lint i=0;i<x;i++)
+#define fj(x) for(lint j=0;j<x;j++)
+#define mod 1000000007
+#define F first
+#define S second
+#define faster ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+
+
+int main(){
+    faster;
+    string a,b;
+    cin>>a>>b;
+    if(a==b){
+        cout<<a;
+    }
+    else{
+        cout<<1;
+    }
+    
+    return 0;
+}
