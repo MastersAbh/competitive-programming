b985179e50981e06beb2aa24aa2ac70463a8de8b
Sahildeep <sahildeepsingh84.ss@gmail.com>
2021-10-05 03:26:01 +0530

    Bipartite graph
    A Bipartite Graph is a graph whose vertices can be divided into two independent sets, U and V such that every edge (u, v) either connects a vertex from U to V or a vertex from V to U. In other words, for every edge (u, v), either u belongs to U and v to V, or u belongs to V and v to U. We can also say that there is no edge that connects vertices of same set.

---
+++ bipartiteGraph.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include<list>
+#include<queue>
+using namespace std;
+
+class Graph{
+    int V;
+    list<int> *l;
+    
+    public:
+        Graph(int v){
+            V = v;
+            l = new list<int>[V];
+            
+        }
+        
+        void addEdge(int u,int v){
+            l[u].push_back(v);
+            l[v].push_back(u);
+        }
+        
+        
+        bool isBipartite(int s){
+            
+            queue<int> q;
+            q.push(s);
+            
+            int *colors = new int[V];
+            for(int i=0;i<V;i++){
+                colors[i] = -1; //Not Visited
+            }
+            
+            colors[s] = 0;
+            
+            while(!q.empty()){
+                
+                int u = q.front();
+                q.pop();
+                
+                for(auto v: l[u]) {
+                    if (colors[v] == -1){
+                        colors[v] = 1 - colors[u];
+                        q.push(v);
+                    }
+                    else if (colors[v] == colors[u]) {
+                        return false;
+                    }   
+                }   
+            } 
+        	return true;
+        }
+        
+};
+
+
+int main() {
+    Graph g(4);
+    g.addEdge(0,1);
+    g.addEdge(2,0);
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(0,3);
+    
+    if(g.isBipartite(0)){
+        cout<<"Yes it is";
+    }
+    else{
+        cout<<"No it is not";
+    }
+}
+
